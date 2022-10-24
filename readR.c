529651ac6eca64c92157245b1efac51e0e8c504d
Alex SYK <alexsyk1502@gmail.com>
2022-10-24 17:29:05 -0300

    modificando

--- readR.c
+++ readR.c
@@ -30,13 +30,11 @@ struct Movimento* readR(int *size){
     
     for(int i = 0; i < *size; i++){
         fscanf(file, "%d %s", &movimento[i].num, &movimento[i].direction);
-        printf("%d ", movimento[i].num);
-        printf("%s \n", movimento[i].direction);
     }
 
-    // for(int i = 0; i < size; i++){
-    //     printf("%d ", movimento[i].num);
-    //     printf("%s\n", movimento[i].direction);
-    // }
+    for(int i = 0; i < *size; i++){
+        printf("%d ", movimento[i].num);
+        printf("%s\n", movimento[i].direction);
+    }
     return movimento;
 }
