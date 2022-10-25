ab8413e558aea2065b42d007cb1c05a3bfffe942
Alex SYK <alexsyk1502@gmail.com>
2022-10-25 14:19:23 -0300

    resolucao pronta

--- main.c
+++ main.c
@@ -1,10 +1,22 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include "read.h"
 
 struct Movimento{
     int num;
     char direction[5];
 };
+
+void printMatrix(int **matrix, int row, int column){
+    printf("######################\n");
+    for (int a = 0; a < row; a++){
+        for (int b = 0; b < column; b++)
+            printf("%d ", matrix[a][b]);
+        printf("\n");           
+    }
+    printf("######################\n");
+}
 
 int main(){
 
@@ -18,7 +30,7 @@ int main(){
     for(i = 0; i < row; i++){
         for(j = 0; j < column; j++){
             if (matrix[i][j] == 0){
-                printf("%d", matrix[i][j]);
+                printf("%d\n", matrix[i][j]);
                 flag = 1;
                 break;
             }
@@ -27,15 +39,41 @@ int main(){
             break;
         }
     }
-    printf("i e j: %d %d", i, j);
+
+    printf("i e j: %d %d\n", i, j);
     //mover os ladrilhos de acordo com o moveset
-    for(int i = 0; i < size; i++){
-        printf("%d ", movimento[i].num);
-        printf("%s \n", movimento[i].direction);
-        if(movimento[i].direction == 'd'){
-            
+    for(int x = 0; x < size; x++){
+        printf("movimento: %d ", movimento[x].num);
+        printf("%s \n", movimento[x].direction);
+        if(strcmp(movimento[x].direction, "d") == 0){ //coluna + 1
+            //verificamos se o movimento[x].num é correspondente
+            aux = matrix[i][j];
+            matrix[i][j] = matrix[i][j-1];
+            matrix[i][j-1] = aux;
+            j--;
+        } else if (strcmp(movimento[x].direction, "e") == 0){ //coluna -1
+            aux = matrix[i][j];
+            matrix[i][j] = matrix[i][j+1];
+            matrix[i][j+1] = aux;
+            j++;
+        } else if (strcmp(movimento[x].direction, "b") == 0){ //linha - 1
+            aux = matrix[i][j];
+            matrix[i][j] = matrix[i-1][j];
+            matrix[i-1][j] = aux;
+            i--;
+        } else if (strcmp(movimento[x].direction, "c") == 0){//linha - 1
+            aux = matrix[i][j];
+            matrix[i][j] = matrix[i+1][j];
+            matrix[i+1][j] = aux;
+            i++;
         }
+        printMatrix(matrix, row, column);
     }
 
+    for (int i = 0; i < column; i++){
+        free(matrix[i]);
+    }
+
+    free(matrix);
     return 0;
 }
